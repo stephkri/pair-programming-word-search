6caa650d74071f7e68871f195038845d98bb2870
stephkri <stephane.krims@hotmail.ca>
2021-08-18 21:37:07 +0000

    Function passes all core tests

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,20 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  const verticalJoin = [];
+  for (let col = 0; col < letters[0].length; col++) {
+    let string = "";
+    for (let row = 0; row < letters.length; row++) {
+      string = string.concat(letters[row][col]);
     }
-}
+    verticalJoin.push(string);
+  }
+  for (const j of verticalJoin) {
+    if (j.includes(word)) return true;
+  }
+  return false;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
